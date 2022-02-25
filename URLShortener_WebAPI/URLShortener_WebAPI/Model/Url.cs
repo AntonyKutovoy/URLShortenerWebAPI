c3761f0c9b99cd6410909e9a7ec04f2ac3048d7d
Антон Кутовой <antonykutovoi@gmail.com>
2022-02-25 11:10:04 +0800

    added url model; removed unnecessary;

---
+++ URLShortener_WebAPI/URLShortener_WebAPI/Model/Url.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace URLShortener_WebAPI.Model
+{
+    public class Url
+    {
+        public string Id { get; set; }
+        public string Original { get; set; }
+        public string Shortened { get; set; }
+        public string VisitCount { get; set; }
+    }
+}
